2d572013421b1c9cbe2944f03145638dd1f055f1
austincammack <acammack75@Bigbend.edu>
2015-01-29 18:40:05 -0800

    I added my project

---
+++ Convertion/Convertion/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Convertion.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Currency Converter</title>
+    </head>
+    <body>
+        <form runat="server">
+          <div>
+            Convert: &nbsp;
+                <input type="text" ID="US" runat="server"/>
+                &nbsp; U.S. dollars to Euros.
+                <br /><br />
+               <input type="submit" value="OK" runat="server" />
+              &nbsp; U.S. dollars to &nbsp;
+              <select ID ="Currency" runat="server"/> 
+               <input type="submit" value="OK" ID="Convert" runat="server"
+                  OnServerClick="Convert_ServerClick" />
+               <br /><br />
+              <p style="font-weight: bold" id="Result" runat="server">
+              </p>
+          </div>
+        </form>
+    </body>
+</html>
---
+++ Convertion/Convertion/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Convertion
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Convert_ServerClick(object sender, EventArgs e)
+        {
+            
+            decimal oldAmount;
+            bool success = Decimal.TryParse(US.Value, out oldAmount);
+            if (success)
+            {
+                ListItem item = Currency.Items[Currency.SelectedIndex];
+                decimal newAmount = oldAmount * Decimal.Parse(item.Value); ;
+                Result.InnerText = oldAmount.ToString() + " U.S. dollars=";
+                Result.InnerText += newAmount.ToString() + " "+ item.Text;
+            }
+            else
+            {
+                Result.InnerText = "The number you typed in was not in the" +
+                    "correct format. Use only numbers.";
+            }
+        }
+      protected void Page_Load(object sender, EventArgs e)
+        {
+          if (this.IsPostBack == false)
+          { Currency.Items.Add(new ListItem("Euro", "0.85"));
+          Currency.Items.Add(new ListItem("Japanese Yen", "110.33"));
+          Currency.Items.Add(new ListItem("Canadian Dollar", "1.2"));
+          
+          }
+        
+        }
+    
+    }
+}
